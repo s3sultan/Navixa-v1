a14bad2b1146710bd6b31c7f910de2cc5ed400ab
s3sultan <s2shug@gmail.com>
2026-09-05 08:34:46 +0300

    Define approved access feature matrix

---
+++ app/accessFeatureMatrix.ts
@@ -0,0 +1,10 @@
+export const ACCESS_FEATURE_MATRIX=[
+  {feature:"المزايا المجانية",free:true,trial:true,azm:true,himma:true},
+  {feature:"مراقبة الشاشة",free:false,trial:true,azm:true,himma:true},
+  {feature:"نداء الاسم",free:false,trial:true,azm:true,himma:true},
+  {feature:"التلخيص",free:false,trial:"limited",azm:false,himma:true},
+  {feature:"ميزات الذكاء",free:false,trial:"limited",azm:false,himma:true},
+  {feature:"NAVIXA English Learning",free:false,trial:"limited",azm:false,himma:true},
+  {feature:"NAVIXA Kids",free:false,trial:"limited",azm:false,himma:true},
+  {feature:"NAVIXA Fitness",free:false,trial:"limited",azm:false,himma:true},
+] as const;
